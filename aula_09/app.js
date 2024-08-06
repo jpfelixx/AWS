5a46ea9c87f29644f01f8d45fe3a3874d6689582
João O. Félix <126785897+jpfelixx@users.noreply.github.com>
2024-08-06 11:32:50 -0300

    Update app.js

--- aula_09/app.js
+++ aula_09/app.js
@@ -1,3 +1,16 @@
 function funcao(){
     alert("usuário deve digitar nota entre 0 e 10")
 }
+
+function confirme(){
+    var x;
+    var r = confirm("sim")
+
+    if(r){
+        x = "OK";
+    }
+    else {
+        x = "NÃO";
+    }
+    document.write("Vocẽ pressionou "+ x);
+}
